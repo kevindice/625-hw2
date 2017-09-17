58a98dd7b3775f564a44b5416100322ef6705313
Kevin Dice <kevin.dice1@gmail.com>
2017-09-16 21:05:58 -0500

    Throw code in a function

--- serial.c
+++ serial.c
@@ -1,10 +1,18 @@
-const int NUM_ITER = 100000000;
-int i;
-double sum = 0.0, x = 0.0;
-double st = 1.0/((double) NUM_ITER);
+#define NUM_ITER 1000000000
 
-for(i = 0; i < NUM_ITER; i++)
-{
-    x = (i + 0.25)*st;
-    sum += 4.0/(x*x+1);
+int i;
+double sum = 0.0;
+double x = 0.0;
+
+double st = 1.0/NUM_ITER;
+
+int main() {
+    for (i = 0; i < NUM_ITER; i++)
+    {
+        x = (i + 0.25)*st;
+        sum += 4.0/(x*x+1);
+    }
+
+    return 0;
 }
+
